a62ac34164981c4b3792e8f414cc98928fc682f6
Johnny Blasius Seubert <68622224+johnnyseubert@users.noreply.github.com>
2022-12-29 19:34:19 -0300

    refactoring cities controller using knex table model

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,20 +1,22 @@
-import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
 import { validation } from "../../shared/middlewares";
+import { StatusCodes } from "http-status-codes";
+import { ICidade } from "../../database/models";
+import { Request, Response } from "express";
 import * as yup from "yup";
 
-interface ICidade {
-   nome: string;
-}
+interface IBodyProps extends Omit<ICidade, "id"> {}
 
 export const createValidation = validation((getSchema) => ({
-   body: getSchema<ICidade>(
+   body: getSchema<IBodyProps>(
       yup.object().shape({
          nome: yup.string().required().min(3),
       })
    ),
 }));
 
-export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+export const create = async (
+   req: Request<{}, {}, IBodyProps>,
+   res: Response
+) => {
    return res.status(StatusCodes.CREATED).json(1);
 };
--- src/server/database/knex/@types/knex.d.ts
+++ src/server/database/knex/@types/knex.d.ts
@@ -1,7 +1,7 @@
+import { ICidade } from "../../models";
+
 declare module "knex/types/tables" {
    interface Tables {
-      // cidade: ICidade;
-      // pessoa: IPessoa;
-      // usuario: IUsuario;
+      cidade: ICidade; // Serve para quando for usar o knex ter autocomplete
    }
 }
