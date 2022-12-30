7145e5404f2a942d6e7e5197a2c8b5f7144893f8
Johnny Blasius Seubert <68622224+johnnyseubert@users.noreply.github.com>
2022-12-30 16:13:31 -0300

    Cities Provider create method

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from "http-status-codes";
 import { ICidade } from "../../database/models";
 import { Request, Response } from "express";
 import * as yup from "yup";
+import { CidadesProvider } from "../../database/providers/cidades";
 
 interface IBodyProps extends Omit<ICidade, "id"> {}
 
@@ -18,5 +19,15 @@ export const create = async (
    req: Request<{}, {}, IBodyProps>,
    res: Response
 ) => {
-   return res.status(StatusCodes.CREATED).json(1);
+   const result = await CidadesProvider.create(req.body);
+
+   if (result instanceof Error) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+         errors: {
+            default: result.message,
+         },
+      });
+   }
+
+   return res.status(StatusCodes.CREATED).json({ id: result });
 };
